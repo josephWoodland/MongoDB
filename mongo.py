5289301dccc6cbab8c1797f1e22bb25a692834e6
Joseph Woodland <joseph@workit-app.com>
2021-08-14 20:16:07 +0100

    Fully functional input user selection

--- mongo.py
+++ mongo.py
@@ -22,7 +22,10 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
-documents = coll.find()
+coll.update_many({'nationality': 'american'},
+ {'$set': {'hair_color': 'maroon'}})
+
+documents = coll.find({'nationality': 'american'})
 
 for doc in documents:
     print(doc)
